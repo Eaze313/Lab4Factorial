3d6be55dc77f70af5cbbff86eeb9110b23a769fe
James Bryant II <jcbryant718@gmail.com>
2017-04-04 01:07:09 -0400

    Added .GitIgnore/Recursion Method/ Continue Loop

--- src/bootcamp/Lab4Factorial.java
+++ src/bootcamp/Lab4Factorial.java
@@ -22,25 +22,43 @@ public class Lab4Factorial {
         Scanner scan = new Scanner(System.in);
 
         String userName = GreetExit.hello(scan);
+        String answer;
 
+    do {
         //Initialize at 1 because 0 multiplication returns 0
         long factorial = 1;
 
         //Get Number From User
+        //Changed to reflect 20 being the largest integer to return a factorial
         int num = Validation.getInt(scan, userName + ", Please enter a " +
-                "number between 1 and 10:\n", 1, 10);
+                "number between 1 and 20:\n", 1, 20);
 
         //Generates Factorial
-        for (int i = num; i > 0; i--) {
-            factorial = factorial * i;
-        }
+        factorial(num);
 
         //Output Results
         System.out.println(userName + ",\nThe factorial of " + num + " is " + factorial);
-        scan.close();
+
+
+        answer = Validation.getString(scan, "Would you like to Continue?" +
+                "\n(Type yes or no)").toLowerCase();
+
+    } while (answer.toLowerCase().charAt(0) == 'y');
 
         //GoodBye
         GreetExit.bye(userName);
+        scan.close();
 
     }
+
+
+
+    public static long factorial(int n) {
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
 }
